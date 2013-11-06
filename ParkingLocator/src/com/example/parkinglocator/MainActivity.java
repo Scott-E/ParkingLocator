0b588afbfc35707f4aca1b82d188dd43b09a8d8a
Scott Elliott <gamesgeeke613@gmail.com>
2013-11-06 17:50:55 -0500

    New array full of costs of parking spots

--- ParkingLocator/src/com/example/parkinglocator/MainActivity.java
+++ ParkingLocator/src/com/example/parkinglocator/MainActivity.java
@@ -25,15 +25,7 @@ public class MainActivity extends Activity {
     
     String address, street, day;
     int startH, endH, deltaH;
-    
-//    private View.OnClickListener onClick = new View.OnClickListener()
-//    {
-//    	@Override
-//    	public void onClick(View v)
-//    	{
-//    		
-//    	}
-//    };
+    double costArray[][] = new double[7][24];
     
     public void onClick()
     {
@@ -53,5 +45,89 @@ public class MainActivity extends Activity {
     	day = spinner.getSelectedItem().toString();
     	EditText viewer = (EditText)findViewById(R.id.address);
     	address = viewer.getText().toString();
+    }
+    
+    public void populateArray()
+    {
+    	int hourCounter = 1;
+    	//Blue Meters
+    	for (int i = 0; i < 24; i++)
+    	{
+    		if(hourCounter <= 3)
+    		{
+    			costArray[0][i] = hourCounter;
+    			hourCounter++;
+    		}
+    		else
+    			costArray[0][i] = 999;
+    	}
+    	hourCounter = 1;
+    	
+    	//Grey Meters
+    	for (int i = 0; i < 24; i++)
+    	{
+    		if(hourCounter <= 1)
+    		{
+    			costArray[1][i] = hourCounter;
+    			hourCounter++;
+    		}
+    		else
+    			costArray[1][i] = 999;
+    	}
+    	hourCounter =  1;
+    	
+    	//Brown Meters
+    	for (int i = 0; i < 24; i++)
+    	{
+    		if(hourCounter <= 10)
+    		{
+    			costArray[2][i] = hourCounter * 0.1;
+    			hourCounter++;
+    		}
+    		else
+    			costArray[2][i] = 999;
+    	}
+    	hourCounter = 1;
+    	
+    	//Yellow Meters
+//    	for (int i = 0; i < 24; i++)
+//    	{
+//    		
+//    	}
+//    	hourCounter = 1;
+    	
+    	//Municipal Garages
+    	for (int i = 0; i < 24; i++)
+    	{
+    		if(hourCounter <= 2)
+    			costArray[3][i] = 0;
+    		else if(hourCounter == 3)
+    			costArray[3][i] = 3;
+    		else if(hourCounter == 4)
+    			costArray[3][i] = 5;
+    		else if(hourCounter == 5)
+    			costArray[3][i] = 7;
+    		else if(hourCounter == 6)
+    			costArray[3][i] = 8;
+    		else if(hourCounter <= 8)
+    			costArray[3][i] = 10;
+    		else if(hourCounter <= 10)
+    			costArray[3][i] = 12;
+    		else if(hourCounter <= 12)
+    			costArray[3][i] = 15;
+    		else
+    			costArray[3][i] = 20;
+    		hourCounter++;
+    	}
+    	hourCounter = 1;
+    	
+    	//Town Center
+    	
+    	
+    	//Corporate Place
+    	
+    	
+    	//Courthouse Place
+    	
     }
 }
